ef93cc05026d25cd111f920751ddfe65dc0dd46d
kaushikmitr <kaushikmitra.umd@gmail.com>
2025-03-24 05:44:34 +0000

    ignore eos while sending non streaming requests to vllm

--- benchmark_serving.py
+++ benchmark_serving.py
@@ -287,7 +287,7 @@ async def send_request(
         "temperature": 0.0 if use_beam_search else 1.0,
         "top_p": 1.0,
         "max_tokens": output_len,
-        "ignore_eos": False,
+        "ignore_eos": True,
         "stream": False,
     }
   elif backend == "tgi":
